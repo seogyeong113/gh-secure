d5ad727498da23036d18040e0a5103ff358196db
seogyeong113 <seogyeong858600@gmail.com>
2026-09-07 03:13:36 +0000

    lab1

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+int main()
+{
+    int score{};
+    char grade{};
+    std::cout<<"Enter score:";
+    std::cin>>score;
+        switch(score/10)
+        {
+            case 10;
+            case 9; grede='A'; break;
+            case 8;
+            case 7;
+            case 6;
+        }
+       
+
+    }
+    // int num0fStudents{30};
+    // if(num0fStudents==30)std::cout<<"default\n";
+    // else std::cout<<"changed\n";
+    // std::cout<<num0fStudents++<<std::endl;
+    // std::cout<<num0fStudents<<td::endl;
+    // std::cout<<++num0fStudents<<std::endl;
+    // std::cout<<num0fStudents<<td::endl;
+
+    // std::cout<<"Enter the numbr0fsyu:";
+    // std::cin>>num0fStudents;
+    // std::cout<<num0fStudents<<std::endl;
+
+    //num0fStudents=32;
+    //std::cout<<typeid(static_cast<double>(num0fStudents)).name()<<std::endl;
+    // std::cout<<num0fStudents<<std::endl;
+    // std::cout<<sizeof(num0fStudents)<<std::endl;
+    // std::cout<<typeid(num0fStudents).name()<<std::endl;
+
+    return 0;
+}
